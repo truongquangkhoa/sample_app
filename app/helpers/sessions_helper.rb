413a09121c0663ec68588464d85e81fdce9c38b2
truongquangkhoa <tk170791@gmail.com>
2016-12-02 09:30:35 +0700

    finish chap 8

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,12 @@ module SessionsHelper
 	end
 
 	def logged_in?
-		!@current_user.nil?
+		# binding.pry
+		!current_user.nil?
+	end
+
+	def log_out
+		session.delete(:user_id)
+		@current_user = nil
 	end
 end
