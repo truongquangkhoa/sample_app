7ac816397d50135e660110ec8e05be361e6df4eb
truongquangkhoa <tk170791@gmail.com>
2016-12-02 15:09:39 +0700

    finish chap 9

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
 	def setup
-		@user = users(:SKT)
+		@user = users(:samsung)
 	end
-    test "login with valiad information" do 
+    test "login with valid information" do 
 	  	get login_path
-	  	post login_path, params: {session: {email: @user.email, password: 'password'} }
+	  	post login_path, params: { session: { email: @user.email, password: 'password' } }
         assert_redirected_to @user
         follow_redirect!
-        asseert_template 'users/show'
+        assert_template 'users/show'
         assert_select "a[href=?]", login_path, count: 0
         assert_select "a[href=?]", logout_path
         assert_select "a[href=?]", user_path(@user)
@@ -18,19 +18,33 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     test "login with valid information followed by logout" do 
     	get login_path
-    	post login_path, params: {session: {email: @user.email,
-    		                                password: 'password'}}
+    	post login_path, params: { session: { email: @user.email,
+    		                                password: 'password' } }
     	assert is_logged_in?
     	assert_redirected_to @user
     	follow_redirect!
+    	assert_template 'users/show'
     	assert_select "a[href=?]", login_path, count: 0
     	assert_select "a[href=?]", logout_path
     	assert_select "a[href=?]", user_path(@user)
     	delete logout_path
-    	assert_not is is_logged_in
+    	assert_not is_logged_in?
     	assert_redirected_to root_url 
+    	#simulate a user clicking logout in a second window.
+    	delete logout_path
+    	follow_redirect!
     	assert_select "a[href=?]", login_path
     	assert_select "a[href=?]", logout_path, count: 0
-    	assert_select "a[href=?]", user_path(u@user), count: 0
+    	assert_select "a[href=?]", user_path(@user), count: 0
+    end 
+
+    test "login with remembering" do 
+    	log_in_as(@user, 'password', 1)
+    	assert_not_nil cookies['remember_token']
+    end 
+
+    test "login without remembering" do 
+    	log_in_as(@user, 'password', 0)
+    	assert_nil cookies['remember_token']
     end 
 end
