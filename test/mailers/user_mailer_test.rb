c0f7028ad1c6eee504df2a242c0c752c563cd83b
truongquangkhoa <tk170791@gmail.com>
2016-12-04 22:16:12 +0700

    finish 12.3

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+
+
+  # def account_activation
+  #   user = User.first
+  #   user.activation_token = User.new_token
+  #   UserMailer.account_activation(user)
+  # end
+
+  # def password_reset
+  #   binding.pry
+  #   user = User.first
+  #   user.reset_token = User.new_token
+  #   UserMailer.password_reset(user)
+  # end
   test "account_activation" do
     user = users(:samsung)
     user.activation_token = User.new_token
@@ -13,12 +27,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escapse(user.mail),  mail.body.encoded
   end
 
-  # test "password_reset" do
-  #   mail = UserMailer.password_reset
-  #   assert_equal "Password reset", mail.subject
-  #   assert_equal ["to@example.org"], mail.to
-  #   assert_equal ["from@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
+  test "password_reset" do
+    user = users(:samsung)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset
+    assert_equal "Password reset", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
+  end
 
 end
